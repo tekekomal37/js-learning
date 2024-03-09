c3adc3835010629cc337dcbbbf17902bc720f05d
tekekomal37 <tekekomal37@gmail.com>
2024-03-09 14:30:44 +0530

    Variable, data type, typeof operator
    Variable, data type, typeof operator

---
+++ 02_typeOf.js
@@ -0,0 +1,43 @@
+console.log("======= Step 1 ========");
+var bankName ;
+console.log("========== Before Initialization===========");
+console.log("Bank Name :",bankName);
+var typeofbankName= typeof bankName;
+console.log("Type of Bank Name :",typeofbankName);
+
+console.log("========== After Initialization===========");
+bankName = "SBI";
+console.log("Bank Name : ",bankName);
+var typeofbankName = typeof bankName;
+console.log("Type of Bank Name :",typeofbankName);
+
+console.log("======= Step 2 ========");
+var mark="90%";
+console.log("Marks :",mark);
+var typeofmark = typeof mark;
+console.log("Type of Mark : ",typeofmark);
+
+console.log("======= Step 3 ========");
+var isWorking;
+console.log("======= Before Initialization ========");
+console.log("Is Working :",isWorking);
+var typeofworking = typeof isWorking;
+console.log("Type of Is Working :",typeofworking);
+
+console.log("======= After Initialization ========");
+isWorking=true;
+console.log("Is Working :",isWorking);
+var typeofworking = typeof isWorking;
+console.log("Type of Is Working :",typeofworking);
+
+console.log("======= Step 4 ========");
+var totalCount = "Hundred and Seven";
+console.log("Total Count :",totalCount);
+var typeofcount = typeof totalCount;
+console.log("Type Of Total Count : ",typeofcount);
+
+console.log("======= Update Variable ========");
+ var totalCount = 107;
+ console.log("Total Count :",totalCount);
+ var typeofcount = typeof totalCount;
+ console.log("Type Of Total Count :",typeofcount);
