4637ce9b222e30cd845dd6f91f5592071b2fdfcc
tekekomal37 <tekekomal37@gmail.com>
2024-03-12 12:47:46 +0530

    String Basic Assignment
    String Basic Assignment

---
+++ 03_stringBasicAssign.js
@@ -0,0 +1,12 @@
+function stringBasics() {
+    var company = "TCS";
+    console.log(`My Dream Company is : ${company}`);
+
+    var hobby1 = "Music Listening";
+    var hobby2 = " Traveling";
+    var hobby3 = " Reading";
+    var hobby= console.log(`My Hobbies are :${hobby1},${hobby2},${hobby3}`);
+    var total = hobby1+hobby2+hobby3;
+    console.log(`Total Character is : ${total.length}`);
+}
+stringBasics();
