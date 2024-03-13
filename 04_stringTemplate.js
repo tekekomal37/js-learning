2a62cf8f17cce35a142555ab1e15b88ff4e2af49
tekekomal37 <tekekomal37@gmail.com>
2024-03-13 18:57:36 +0530

    String Template
    String Template

--- 04_stringTemplate.js
+++ 04_stringTemplate.js
@@ -37,5 +37,45 @@ var str= "developers world! ;"
 console.log(str.slice(0,10));
 
 
+console.log(`========== trim() ==================`);
+var greet = "   Good Morning   ";
+var lengthBeforeTrim = greet.length;
+console.log(` ${greet} it's length is : ${lengthBeforeTrim} `);
 
+var greetAfterTrim = greet.trim();
+var lengthAfterTrim = greetAfterTrim.length;
+console.log(` ${greetAfterTrim} it's length is : ${lengthAfterTrim} `);
+console.log(`Total spaces removed is: ${lengthBeforeTrim-lengthAfterTrim}`);
+
+
+console.log(`========== includes() ==================`);
+var greet = "Good Morning";
+var result = greet.includes("nin");
+console.log(`Is string ${greet} includes word or char 'nin': ${result}`);
+
+var result = greet.includes("MoR");
+console.log(`Is string ${greet} includes word or char 'MoR': ${result}`);
+
+console.log(`========== search() ==================`);
+var greet = "Good Morning";
+var result = greet.search("Morning");
+console.log(`'Morning' is available at ${result}`);
+
+var result = greet.search("r");
+console.log(`'r' is available at ${result}`);
+
+console.log(`========== split() ==================`);
+var greet = "Good Morning";
+var resultValue = greet.split(" ")
+console.log(resultValue);
+console.log(`Total words are: ${resultValue.length}`);
+
+function totalWord(arg1) {
+     
+    var result = arg1.split(" ")
+    console.log(result);
+    console.log(`Total Number Of Words  : ${result.length}`);
+     
+}
+totalWord("I am happy Buddy");
 
