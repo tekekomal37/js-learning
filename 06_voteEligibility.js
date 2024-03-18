46a113b3130544545a2580cc4298b3c379f818ef
tekekomal37 <tekekomal37@gmail.com>
2024-03-18 17:15:43 +0530

    Voting Criteria Assignment
    Voting Criteria Assignment

---
+++ 06_voteEligibility.js
@@ -0,0 +1,20 @@
+var voteEligible = function (age) {
+    if (age >= 18 ) {
+        console.log(`Age ${age} : Eligible for Vote`);
+    } else if (age < 18){
+        console.log(`Age ${age} : Not Eligible for vote`);
+    }else if(age=0 || age<=0 || age>=130){
+        console.log(`Age ${age} : Invalid Data`);
+    }else{
+        console.log("Information Not Found");
+    }
+    
+}
+ voteEligible(45);
+ voteEligible(17); 
+voteEligible(8); 
+ voteEligible(20);
+voteEligible(-10); 
+voteEligible(200); 
+voteEligible(0); 
+voteEligible(null); 
