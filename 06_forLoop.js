b1f7d0c54a68207daf6de07683434991a895bd7e
tekekomal37 <tekekomal37@gmail.com>
2024-03-19 16:05:45 +0530

    for loop
    for loop

---
+++ 06_forLoop.js
@@ -0,0 +1,26 @@
+console.log(`// WAP to print numbers from 0 to 10`);
+for (let index = 0; index <=10; index++) {
+   console.log(index);
+    
+}
+
+console.log(`// WAP to print numbers from 15 to 20`);
+for (let index = 15; index <=24; index++) {
+    console.log(index);
+    
+}
+console.log(`// WAP to print numbers from 10 to 5`);
+for (let index = 10; index >=5; index--) {
+    console.log(index);
+    
+}
+
+console.log(` WAP to print event numbers from 0 to 20`);
+for (let index = 0; index <=20; index=index+2) {
+    console.log(index); 
+}
+
+console.log(` WAP to print ODD numbers from 15 to 29`);
+for (let index = 15; index <=29; index=index+2) {
+    console.log(index); 
+}
