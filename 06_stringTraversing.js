3f11b510e0fef606623d7fee3451898673b617aa
tekekomal37 <tekekomal37@gmail.com>
2024-03-19 16:06:12 +0530

    String Traversing
    String Traversing

---
+++ 06_stringTraversing.js
@@ -0,0 +1,17 @@
+var word =" Javascript";
+for (let index = 0; index < word.length; index++) {
+   var element = word.charAt(index);
+   console.log(element);
+}
+
+
+console.log(`==== WAP to count the char a ===`);
+var word = "JavaScript";
+var count = 0;
+for (let index = 0; index<word.length; index++) {
+    var char = word.charAt(index);
+    if (char=='a') {
+        count = count+1;
+    }  
+}
+console.log(`Char a count is: ${count}`);
