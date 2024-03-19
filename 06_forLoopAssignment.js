9c7cbcd71fea34902f84bc9dc469c6761b67ad9d
tekekomal37 <tekekomal37@gmail.com>
2024-03-19 16:06:51 +0530

    For loop Assignment
    For loop Assignment

---
+++ 06_forLoopAssignment.js
@@ -0,0 +1,41 @@
+console.log("WAP for print number from 5 to 15 incrementing by 1");
+for (let index = 5; index <=15; index++) {
+    console.log(index);
+    
+}
+
+console.log("WAP for print number from 50 to 40 decrementing by 1");
+for (let index = 50; index>=40; index--) {
+   console.log(index);
+    
+}
+
+console.log("WAP for First 15 odd Number");
+for (let index = 1; index <=30; index = index+2) {
+    console.log(index);
+    
+}
+
+console.log("WAP for First 10 Even Number");
+for (let index = 2; index <=20; index = index+2) {
+    console.log(index);
+    
+}
+
+console.log("WAP for Table of 5");
+for (let index=5; index <=50; index = index+5) {
+    console.log(index);
+    
+}
+
+console.log("WAP for Table of 10");
+for (let index=10; index <=100; index = index+10) {
+    console.log(index);
+    
+}
+
+console.log("WAP for Table of 10 in Reverse Order");
+for (let index=100; index >=10; index = index-10) {
+    console.log(index);
+    
+}
