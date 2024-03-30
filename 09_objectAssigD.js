36cd7950f4783bbec66c88be3dbfb5ce2a440895
tekekomal37 <tekekomal37@gmail.com>
2024-03-30 16:37:16 +0530

    Object Assignment
    Object Assignment

---
+++ 09_objectAssigD.js
@@ -0,0 +1,73 @@
+let professor = {
+    name:"Mick",
+    Age :45,
+    city:"Pune",
+    Specialize:"Java",
+    Salary : 45000,
+    degree : {
+        Engineering :"CSC",
+        PHP : "Adv Computing",
+        getdegree : function() {
+            const degree = `Engineering : ${this.Engineering}, PHP : ${this.PHP}`
+            return degree;
+        }
+        
+    },
+    Certificates:["Hacker rank Participation","Certificate in IFE Course","Certificate in Adv Programming"]
+
+}
+console.log("1 Add Properties of the Professor");
+console.log("2 Include Nested object degree");
+console.log("3 Add One Array Certificate");
+console.log(professor);
+
+  console.log("4 Add new Property Experiencce");
+  professor.totalExperience = "14 Years";
+  console.log(professor);
+
+console.log("5 Modifying one property");
+professor.Age = 35;
+console.log(professor);
+
+console.log("6-Add one new Certificate");
+// var addCertificate = professor.Certificates.splice(2,0,"Oracle Certifi");
+// console.log(addCertificate);
+// professor.Certificates = "Oracle Certified";
+// console.log(Certificates);
+
+// Existing object
+// let professor = {
+    // Certificates: ["First Certificate"] // Assuming certificate property is an array
+//   };
+  
+  // Adding a second value to "certificate" property using splice method
+  if (!Array.isArray(professor.Certificates)) {
+    console.error("Certificate property is not an array.");
+  } else {
+    // Inserting the second certificate at index 1
+    professor.Certificates.splice(2, 0, "Oracle Certified");
+  }
+  
+  // Output the updated object
+  console.log(professor);
+  console.log(professor.Certificates);
+
+  console.log("7 Log the last Element array - Certificate");
+  let len = professor.Certificates.length;
+  console.log(professor.Certificates[len-1]);
+
+  console.log("8 log Complete Object on Console");
+  console.log(professor);
+
+  console.log("Traverse array - certificates using For of loop");
+    for (let index = 0; index < professor.Certificates.length; index++) {
+      const element = professor.Certificates[index];
+      console.log(element);
+    }
+
+//  const  arrayNums = [2, 4, 1, 7, 9, 8];
+// for (let index = 0; index < arrayNums.length; index++) {
+//     const element = arrayNums[index];
+//     console.log(element);
+// }
+  
